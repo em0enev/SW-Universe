89527562f9ab93e9b94dc118f59d4d3063073144
Emil Enev <emil.enev@abv.bg>
2021-09-09 22:50:03 +0300

    fix: code refactoring

--- src/app/custom/StarWarsUniverse.js
+++ src/app/custom/StarWarsUniverse.js
@@ -1,19 +1,14 @@
 import Entity from "./Entity";
 
-export default class StarWarsUniverse{
-    constructor(){
+export default class StarWarsUniverse {
+    constructor() {
         this.entities = []
     }
 
-    async init(){
+    async init() {
         const API_ROOT = 'https://swapi.boom.dev/api/';
 
-        const response = await fetch(API_ROOT);
-        const data = await response.json()
-
-        for (const [k, v] of Object.entries(data)) {
-            let entity = new Entity(k, v)
-            this.entities.push(entity)
-        }
+        const data = await fetch(API_ROOT);
+        return data.json()
     }
 }
