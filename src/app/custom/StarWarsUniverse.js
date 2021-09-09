b611b44c12b81d87ca3f10c42eb5c1f10710a5ea
Emil Enev <emil.enev@abv.bg>
2021-09-09 22:22:37 +0300

    feat: implement StarWarsUniverse

---
+++ src/app/custom/StarWarsUniverse.js
@@ -0,0 +1,20 @@
+import Entity from "./Entity";
+
+export default class StarWarsUniverse{
+    constructor(){
+        this.entities = []
+    }
+
+    async init(){
+        const API_ROOT = 'https://swapi.boom.dev/api/';
+
+        const response = await fetch(API_ROOT);
+        const data = await response.json()
+
+        for (const [k, v] of Object.entries(data)) {
+            let entity = new Entity(k, v)
+            this.entities.push(entity)
+        }
+        console.log(this.entities)
+    }
+}
