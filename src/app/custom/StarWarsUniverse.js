32f8ba2815b3e718155adf0a3ba848493319d42e
Emil Enev <emil.enev@abv.bg>
2021-09-14 10:06:43 +0300

    fix: fetching the right data

--- src/app/custom/StarWarsUniverse.js
+++ src/app/custom/StarWarsUniverse.js
@@ -2,13 +2,21 @@ import Entity from "./Entity";
 
 export default class StarWarsUniverse {
     constructor() {
-        this.entities = []
+        this.entities = [];
     }
 
     async init() {
         const API_ROOT = 'https://swapi.boom.dev/api/';
 
         const data = await fetch(API_ROOT);
-        return data.json()
+        const dataFromApi = await data.json()
+
+        for (const [k, v] of Object.entries(dataFromApi)) {
+            const res = await fetch(v)
+            const entityData = await res.json();
+
+            let entity = new Entity(k, entityData)
+            this.entities.push(entity)
+          }
     }
 }
